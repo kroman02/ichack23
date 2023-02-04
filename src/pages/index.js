f654108700cfcbd0195986a42abd41ff07fa08af
Amy Ann Franz <amy.franz@gmail.com>
2023-02-04 23:55:57 +0000

    tailwind added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,24 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>aMeal.io</title>
-        <meta name="description" content="aMeal.io is a the meal prep inspiration app without the bs" />
+        <meta
+          name="description"
+          content="aMeal.io is a the meal prep inspiration app without the bs"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link href="/dist/output.css" rel="stylesheet" />
       </Head>
-      <main>
-      </main>
+      <main className="bg-white"></main>
     </>
-  )
+  );
 }
