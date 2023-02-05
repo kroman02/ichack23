1f3372f7e3a7c722492090787f78f88e76855dd3
Amy Ann Franz <amy.franz@gmail.com>
2023-02-05 02:37:22 +0000

    checkboxes

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,6 @@ import { Inter } from "@next/font/google";
 import Header from "components/header";
 import Rectangle from "components/Rectangle";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
     <>
