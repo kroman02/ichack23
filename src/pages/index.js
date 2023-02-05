4b0fc2ea143b8af53915fe2bb337aeaf233c5238
Amy Ann Franz <amy.franz@gmail.com>
2023-02-05 00:08:46 +0000

    min width and height

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-bcream"><p className="text-bblue">Hello World</p></main>
+      <main className="bg-bcream min-w-screen min-h-screen"><p className="text-bblue">Hello World</p></main>
     </>
   );
 }
