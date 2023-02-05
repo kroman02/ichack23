ff00b48a7145f7b1f4614eaf90c00b9facef981e
JulieVand <115578777+JulieVand@users.noreply.github.com>
2023-02-05 01:18:59 +0000

    Added rectangle

---
+++ components/Rectangle.js
@@ -0,0 +1,7 @@
+
+export default function Rectangle() {
+  return (
+    <div class="rounded-tr-[15rem] w-5/6 h-1/3 absolute bottom-0 left-0 bg-bblue"></div>
+    
+  );
+}
--- components/header.js
+++ components/header.js
@@ -3,7 +3,7 @@ import Logo from "./logo.js";
 export default function Header() {
   return (
     <>
-      <Logo styleComp="w-10 h-10 text-borange" />
+      <Logo styleComp="w-10 h-10 m-3 text-borange" />
       <p></p>
     </>
   );
