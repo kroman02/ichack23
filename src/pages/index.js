05779587aba64bf3bca83c3d1cb26c07cd423c4c
Amy Ann Franz <amy.franz@gmail.com>
2023-02-05 00:07:27 +0000

    brand colours added to tailwind

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,9 +16,8 @@ export default function Home() {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <link href="/dist/output.css" rel="stylesheet" />
       </Head>
-      <main className="bg-white"></main>
+      <main className="bg-bcream"><p className="text-bblue">Hello World</p></main>
     </>
   );
 }
